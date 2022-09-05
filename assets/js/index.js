5ccac6b450a005bbed915492038e2967dfe7f87a
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-09-06 01:31:18 +0600

    about responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,15 @@
 
-const openMenu = document.getElementById('open-menu').addEventListener('click', function(){
+const openMenu = document.getElementById('open-menu').addEventListener('click', function () {
     let menu = document.getElementById('nav-menu')
+    menu.classList.remove('d-none');
     menu.style.right = '0';
-    menu.style.visibility = "visible";    
+    menu.style.transition = '.8s'
+
 });
-const closeMenu = document.getElementById('close-menu').addEventListener('click', function(){
+const closeMenu = document.getElementById('close-menu').addEventListener('click', function () {
     let menu = document.getElementById('nav-menu')
+    menu.classList.add('d-none');
     menu.style.right = '-100vw';
-    menu.style.visibility = "hidden";   
+    menu.style.transition = '.8s'
+
 });
